40ece693d79f1818a01db6b4db9087dcce7f73d7
Cassandra Mathison <cassiemathison@yahoo.com>
2023-10-31 10:36:51 -0700

    Add story struct, stories array, updateUI based on button selection

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,8 +14,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    //private var story: Story!
+   
+    var currentStory = 0
+    
+    
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        if sender.currentTitle == stories[currentStory].choice1 {
+            currentStory = 1
+            print("choice1Button")
+        } else {
+            currentStory = 2
+            print("choice2Button")
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = stories[currentStory].title
+        
+        choice1Button.setTitle(stories[currentStory].choice1, for: .normal)
+        
+        choice2Button.setTitle(stories[currentStory].choice2, for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
+       
 
     }
 
