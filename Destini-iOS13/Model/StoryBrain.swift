d07caf8ea2107908a8949db09c1ac81b1fa0e85c
Cassandra Mathison <cassiemathison@yahoo.com>
2023-11-02 13:45:26 -0700

    Implement MVC

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,39 @@
 
 import Foundation
 
+struct StoryBrain {
+    
+    var currentStory = 0
+    
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    mutating func nextStory(userChoice: String) {
+        if userChoice == stories[currentStory].choice1 {
+            currentStory = 1
+            print("choice1Button")
+        } else {
+            currentStory = 2
+            print("choice2Button")
+        }
+        
+    }
+    
+    
+    func getTitle() -> String {
+        return stories[currentStory].title
+    }
+    
+    func getChoiceOne() -> String {
+        return stories[currentStory].choice1
+    }
+    
+    func getChoiceTwo() -> String {
+        return stories[currentStory].choice2
+    }
+}
+
 
